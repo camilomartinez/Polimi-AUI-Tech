02e927e74cc6d29361289421c35af89ce2408e12
Camilo Martinez <marscamand@gmail.com>
2014-11-10 22:50:18 +0100

    Load movies metadata function

--- custom-functions.R
+++ custom-functions.R
@@ -13,3 +13,14 @@ mostPopularItems <- function(URM) {
   # Only output movies as a vector
   mostPopularMovies <- as.vector(itemsByPopularity[["ItemId"]])
 }
+
+loadMovies <- function() {
+  # Get movie metadata
+  movies <- read.csv("movieMetaCorrected.csv")
+  # Split genre string by '|' grouping by MovieId
+  splitGenre = function(row) {
+    strsplit(as.character(row$Genre), '|', fixed=TRUE)
+  }
+  movies$Genre <- by(movies, movies$MovieId,FUN=splitGenre)
+  return(movies)
+}
