e4962f804972b213250ec239ef59b7bc7c5e4d74
Camilo Martinez <marscamand@gmail.com>
2014-11-17 21:59:33 +0100

    Using mean average precision for evaluation

--- modified-recommender-lab.R
+++ modified-recommender-lab.R
@@ -25,32 +25,33 @@ setMethod("calcPredictionAccuracy", signature(x= "topNList",
               res <- colMeans(res, na.rm=TRUE)
               # Computing Mean Average Precision from Metrics Package
               # and properly appending to results
-              #predicted <- as(x,"list")
-              #n <- max(sapply(predicted, length))
-              #actual <- as(data,"list")
-              #mapVector <- c("MAP"=mapk(n, actual, predicted))
-              #map <- mapk(n, actual, predicted)
-              #res <- append(res, map)
-            }
-            
+              predicted <- as(x,"list")
+              n <- max(sapply(predicted, length))
+              actual <- as(data,"list")
+              map <- mapk(n, actual, predicted)
+              # Add metric with name
+              res <- append(res, map)
+              names(res)[length(res)] <- "MAP"
+            }       
             res
           })
 
-# apk <- function(k, actual, predicted)
-# {
-#   score <- 0.0
-#   if ((length(predicted) > 0) && (k > 0))
-#   {
-#     cnt <- 0.0
-#     for (i in 1:min(k,length(predicted)))
-#     {
-#       if (predicted[i] %in% actual && !(predicted[i] %in% predicted[0:(i-1)]))
-#       {
-#         cnt <- cnt + 1
-#         score <- score + cnt/i 
-#       }
-#     }
-#     score <- score / min(length(actual), k)
-#   }
-#   score
-# }
+myApk <- function(k, actual, predicted)
+{
+  score <- 0.0
+  if ((length(predicted) > 0) && (k > 0))
+  {
+    cnt <- 0.0
+    for (i in 1:min(k,length(predicted)))
+    {
+      if (predicted[i] %in% actual && !(predicted[i] %in% predicted[0:(i-1)]))
+      {
+        cnt <- cnt + 1
+        score <- score + cnt/i 
+      }
+    }
+    score <- score / min(length(actual), k)
+  }
+  score
+}
+assignInNamespace("apk", myApk, ns="Metrics")
