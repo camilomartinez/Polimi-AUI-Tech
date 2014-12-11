dc1cbd313a093a2c1bd1fa3ff8176ab9b793c2f4
Camilo Martinez <marscamand@gmail.com>
2014-12-11 11:14:46 +0100

    Correcting some errors with the execution

--- analyze-ar-model.R
+++ analyze-ar-model.R
@@ -1,11 +1,18 @@
 library(recommenderlab)
 
 source("modified-packages.R")
+## register recommender
+recommenderRegistry$delete_entry(method="AR", dataType = "realRatingMatrix")
+recommenderRegistry$set_entry(
+  method="AR", dataType = "realRatingMatrix", fun=REAL_AR,
+  description="Recommender based on association rules.",
+  parameters=.REAL_AR_param
+)
 source("custom-functions.R")
 
 trainCsv = ReadCsvData("train")
 urm <- as(trainCsv, "realRatingMatrix")
-recommender <- Recommender(urm, method = "AR", param=list(measure="hyperConfidence", maxlen=2))
+recommender <- Recommender(urm, method = "AR", param=list(measure="confidence", support=0.02, maxlen=2))
 # Get association rules from model
 rules <- (getModel(recommender)$rule_base)
 # Display statistics
